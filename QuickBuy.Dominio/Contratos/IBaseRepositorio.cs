7de2bc6f3fa801a175c909767fa82016a7045f7a
Lucas Barros Bezerra <lucasbabezerra@gmail.com>
2021-03-21 16:29:10 -0300

    Crição de algumas interfaces

---
+++ QuickBuy.Dominio/Contratos/IBaseRepositorio.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace QuickBuy.Dominio.Contratos
+{
+    //                                  Está definindo que a interface IBaseRposositori vai trabalhar apenas com TEntity
+    //                                  A interface IBaseRposositori herda de outra interface a IDisposable
+    //                                  Esta dizendo que TEntity é um class
+    public interface IBaseRepositorio<TEntity> : IDisposable where TEntity : class
+    {
+        void Adicionar(TEntity entity);
+        TEntity ObterId(int id);
+        IEnumerable<TEntity> ObterTodos();
+        void Atualizar(TEntity entity);
+        void Remover(TEntity entity);
+    }
+}
