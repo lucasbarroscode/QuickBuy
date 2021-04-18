bbeeeb924185cc47ab33272f3769767f676da2a8
Lucas Barros Bezerra <lucasbabezerra@gmail.com>
2021-04-18 17:11:10 -0300

    Criando as classes de mapeamento

--- QuickBuy.Repositorio/Contexto/QuickBuyContexto.cs
+++ QuickBuy.Repositorio/Contexto/QuickBuyContexto.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using QuickBuy.Dominio.Entidades;
 using QuickBuy.Dominio.ObjetoDeValor;
+using QuickBuy.Repositorio.Config;
 
 namespace QuickBuy.Repositorio.Contexto
 {
@@ -16,6 +17,17 @@ namespace QuickBuy.Repositorio.Contexto
         public QuickBuyContexto(DbContextOptions options) : base(options)
         {
 
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            ///Classe de mapeamento aqui
+            modelBuilder.ApplyConfiguration(new UsuarioConfiguration());
+            modelBuilder.ApplyConfiguration(new ProdutoConfiguration());
+            modelBuilder.ApplyConfiguration(new PedidoConfiguration());
+            modelBuilder.ApplyConfiguration(new ItemPedidoConfiguration());
+            modelBuilder.ApplyConfiguration(new FormaPagamentoConfiguration());
+            base.OnModelCreating(modelBuilder);
         }
 
 
